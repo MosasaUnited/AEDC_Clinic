61bf7a9c28516a32fdead89b64812b4fe550ffee
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-07-03 14:14:53 +0300

    CommissionViewBodyUIFinished

--- lib/features/commission/presentation/commission_view.dart
+++ lib/features/commission/presentation/commission_view.dart
@@ -1,14 +1,23 @@
 import 'package:aedc_clinic/features/commission/presentation/screens/commission_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CommissionView extends StatelessWidget {
   const CommissionView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: CommissionViewBody(),
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              GoRouter.of(context).pop();
+            },
+            icon: const Icon(Icons.arrow_back_ios_new),
+          ),
+        ),
+        body: const CommissionViewBody(),
       ),
     );
   }
