b1f4ceba0b436a71bc5ed7069eb57f7d13c3eff9
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-07-04 14:00:26 +0300

    RefactorCommissionViewBody

--- lib/features/commission/presentation/screens/commission_view_body.dart
+++ lib/features/commission/presentation/screens/commission_view_body.dart
@@ -2,7 +2,9 @@ import 'package:aedc_clinic/core/theme/styles.dart';
 import 'package:aedc_clinic/features/commission/presentation/widgets/paper_must_have.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../../../core/constants/svg_assets.dart';
 import '../../../../core/widgets/custom_button_animation.dart';
 import '../widgets/important_notice.dart';
 
@@ -13,29 +15,43 @@ class CommissionViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          const PaperMustHave(),
-          SizedBox(
-            height: 30.h,
-          ),
-          const ImportantNotice(),
-          const Spacer(),
-          Center(
-            child: CustomButtonAnimation(
-              onPressed: () {},
-              child: Text(
-                textAlign: TextAlign.center,
-                'اختار ميعاد لتقديم الأوراق المطلوبة',
-                style: Styles.textStyle20.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w900,
-                    fontSize: 15.sp),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Center(
+              child: SvgPicture.asset(
+                SvgAssets.commissionSvg,
+                height: 120.h,
+                width: 200.w,
               ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 20.h,
+            ),
+            const PaperMustHave(),
+            SizedBox(
+              height: 20.h,
+            ),
+            const ImportantNotice(),
+            SizedBox(
+              height: 50.h,
+            ),
+            Center(
+              child: CustomButtonAnimation(
+                onPressed: () {},
+                child: Text(
+                  textAlign: TextAlign.center,
+                  'اختار ميعاد لتقديم الأوراق المطلوبة',
+                  style: Styles.textStyle20.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 15.sp),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
