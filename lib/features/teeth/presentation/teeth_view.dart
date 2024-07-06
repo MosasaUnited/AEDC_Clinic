5d1f8f2a1805873750f2ef83530887f6a8230e63
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-07-06 11:09:25 +0300

    FinishedTeethViewBody

--- lib/features/teeth/presentation/teeth_view.dart
+++ lib/features/teeth/presentation/teeth_view.dart
@@ -1,14 +1,19 @@
 import 'package:aedc_clinic/features/teeth/presentation/screens/teeth_view_body.dart';
 import 'package:flutter/material.dart';
 
+import '../../../core/widgets/simple_appbar.dart';
+
 class TeethView extends StatelessWidget {
   const TeethView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
         child: Scaffold(
-      body: TeethViewBody(),
+      appBar: AppBar(
+        leading: const SimpleAppbar(),
+      ),
+      body: const TeethViewBody(),
     ));
   }
 }
