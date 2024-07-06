3e79d748eba3db99b86a53120f91f76d55bd4812
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-07-06 11:20:23 +0300

    StartingXrayViewBody

--- lib/features/x-ray/presentation/screens/x-ray_view_body.dart
+++ lib/features/x-ray/presentation/screens/x-ray_view_body.dart
@@ -1,12 +1,176 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
-class XrayViewBody extends StatelessWidget {
+import '../../../../core/constants/svg_assets.dart';
+import '../../../../core/theme/styles.dart';
+import '../../../../core/widgets/custom_button_animation.dart';
+
+class XrayViewBody extends StatefulWidget {
   const XrayViewBody({super.key});
 
   @override
+  State<XrayViewBody> createState() => _XrayViewBodyState();
+}
+
+class _XrayViewBodyState extends State<XrayViewBody> {
+  final pickTimeController = TextEditingController();
+
+  final pickDateController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  final List<String> dropDownItems = [
+    '',
+    'دار الأسعة',
+    'مركز القلب و الصدر',
+    'ICC Scan',
+  ];
+
+  String selectedTextFieldItem = '';
+  @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    return Form(
+      key: formKey,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SvgPicture.asset(
+                      SvgAssets.xraySvg,
+                      height: 120.h,
+                      width: 400.w,
+                    ),
+                    SizedBox(
+                      height: 50.h,
+                    ),
+                    DropdownButtonFormField(
+                      value: selectedTextFieldItem,
+                      validator: (value) {
+                        if (value == '') {
+                          return 'من فضلك اختر مركز الأشعة';
+                        }
+                        return null;
+                      },
+                      items: dropDownItems.map((String item) {
+                        return DropdownMenuItem(
+                          value: item,
+                          child: Center(child: Text(item)),
+                        );
+                      }).toList(),
+                      onChanged: (String? value) {
+                        setState(() {
+                          selectedTextFieldItem = value!;
+                        });
+                      },
+                      icon: const Icon(Icons.arrow_drop_down),
+                      decoration: const InputDecoration(
+                        labelText: ' -- إختر مركز الأشعة --',
+                        labelStyle: TextStyle(
+                          fontWeight: FontWeight.w800,
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    TextFormField(
+                      controller: pickDateController,
+                      decoration: const InputDecoration(
+                        hintText: 'إختر اليوم',
+                        border: OutlineInputBorder(),
+                      ),
+                      readOnly: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'من فضلك اختر اليوم';
+                        }
+                        return null;
+                      },
+                      onTap: () async {
+                        final selectDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime.parse('2038-01-01'),
+                        ).then((value) {
+                          pickDateController.text =
+                              DateFormat.yMMMd().format(value!);
+                        });
+                        if (selectDate != null) {
+                          print(selectDate);
+                          final formattedDate = selectDate.format(context);
+                          pickDateController.text = formattedDate;
+                        } else {
+                          print('Date not selected');
+                        }
+                      },
+                    ),
+                    SizedBox(height: 20.h),
+                    TextFormField(
+                      controller: pickTimeController,
+                      decoration: const InputDecoration(
+                          hintText: 'إختر الوقت', border: OutlineInputBorder()),
+                      readOnly: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'من فضلك اختار الوقت';
+                        }
+                        return null;
+                      },
+                      onTap: () async {
+                        final selectTime = await showTimePicker(
+                          context: context,
+                          initialTime: TimeOfDay.now(),
+                        );
+                        if (selectTime != null) {
+                          print(selectTime);
+                          final formattedTime = selectTime.format(context);
+                          pickTimeController.text = formattedTime;
+                        } else {
+                          print('Time is Not Selected');
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              CustomButtonAnimation(
+                onPressed: () async {
+                  if (formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text(
+                          'تم حجز الميعاد بنجاح',
+                          style: TextStyle(
+                              color: Colors.white,
+                              decorationStyle: TextDecorationStyle.double),
+                        ),
+                        backgroundColor: Colors.green,
+                      ),
+                    );
+                  }
+                },
+                child: Text(
+                  textAlign: TextAlign.center,
+                  'إحجز ميعاد',
+                  style: Styles.textStyle20.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
